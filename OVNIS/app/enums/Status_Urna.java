4369cb68389d8c4015738c9b675cc51ec4cebaef
viniciusfcar <38993929+viniciusfcar@users.noreply.github.com>
2019-01-05 14:04:27 -0200

    Add files via upload
    enums atualizados

---
+++ OVNIS/app/enums/Status_Urna.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum Status_Urna {
+    
+    ATIVADA,
+    DESATIVADA,
+    VINCULADA,
+    DESVINCULADA
+}
---
+++ OVNIS/app/enums/Status_Voto.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum Status_Voto {
+    
+    NAO_VOTOU,
+    VOTOU,        
+    JUSTIFICOU
+}
